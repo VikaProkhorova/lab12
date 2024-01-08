058e0e68dcfe4b6b33301efc97ffe7fd470850dd
VikaProkhorova <116552538+VikaProkhorova@users.noreply.github.com>
2024-01-08 14:44:51 +0300

    Add files via upload

---
+++ src/main/java/atm/Handler50.java
@@ -0,0 +1,16 @@
+package atm;
+
+public class Handler50 extends Handler{
+    @Override
+    public void process(int price) {
+        Handler next = getNext();
+        if (price % 50 > 0 && next == null) {
+            throw new IllegalArgumentException();
+        } else if (next != null){
+            getNext().process(price % 50);
+        }
+        System.out.println(price / 50 + " * 50");
+
+
+    }
+}
---
+++ src/main/java/decorator/TimedDocument.java
@@ -0,0 +1,22 @@
+package decorator;
+
+import static java.lang.System.currentTimeMillis;
+
+public class TimedDocument implements Document{
+    Document document;
+    public TimedDocument(Document document) {
+        this.document = document;
+    }
+
+    public long measureTime(){
+        long start = System.currentTimeMillis();
+        this.parse();
+        return System.currentTimeMillis() - start;
+    }
+
+
+    @Override
+    public String parse() {
+        return document.parse();
+    }
+}
